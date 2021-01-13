8880b3410253123d08a4199f742d91b5155b7bfe
IND_Solimar Milagros Huayhua Malaga <xt2648@rimac.com.pe>
2021-01-12 19:33:09 -0500

    Updates

--- src/redux/reducers/index.js
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from "redux";
-import results from "./results";
-
-export default combineReducers({ results });
--- src/utils/keys.js
+++
@@ -1,2 +0,0 @@
-export const google_key = 'AIzaSyDKGV7naNqKQvkcbpl6-X4NgRO6eC3caQg';
-export const bing_key = 'b89c6a5ac03843fc9f2e6f7727ace7ea';
